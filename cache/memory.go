770f193bdf8f4e8ce10f09a9dddd1484cd1cfb01
Jordan Mance <jordan.mance@zendesk.com>
2024-08-26 14:47:37 -0400

    Allowing  memory cache to be disabled by providing TTL of 0 or capacity of <=0

--- cache/memory.go
+++ cache/memory.go
@@ -43,6 +43,11 @@ func NewInMemoryCache[K comparable, V any](ttl time.Duration, opts ...CacheOptio
 
 func (c *InMemoryCache[K, V]) Get(key K) (V, bool, error) {
 	var v V
+	if c.cacheIsDisabled() {
+		// TTL is zero, or capacity is 0, we should always miss
+		return v, false, nil
+	}
+
 	item := c.cache.Get(key)
 	if item != nil {
 		return item.Value(), true, nil
@@ -68,6 +73,11 @@ func (c *InMemoryCache[K, V]) Get(key K) (V, bool, error) {
 }
 
 func (c *InMemoryCache[K, V]) Set(key K, val V) error {
+	if c.cacheIsDisabled() {
+		// TTL is zero, or capacity is 0, we should always miss
+		return nil
+	}
+
 	_ = c.cache.Set(key, val, c.ttl)
 
 	// if fail-through is enabled, set in fail-thorugh
@@ -105,6 +115,12 @@ func (c *InMemoryCache[K, V]) Purge() error {
 }
 
 func (c *InMemoryCache[K, V]) GetOrSet(key K, orSet func() (V, error)) (val V, wasFoundInCache bool, err error) {
+	var v V
+	if c.cacheIsDisabled() {
+		// TTL is zero, or capacity is 0, we should always miss
+		return v, false, nil
+	}
+
 	item, wasFound, err := c.Get(key)
 	if wasFound {
 		return item, wasFound, err
@@ -123,4 +139,8 @@ func (c *InMemoryCache[K, V]) GetOrSet(key K, orSet func() (V, error)) (val V, w
 	// Error on SET, so we wrap it in CacheSetError
 	err = CacheSetError{Message: err.Error()}
 	return val, false, err
+}
+
+func (c *InMemoryCache[K, V]) cacheIsDisabled() bool {
+	return c.ttl == 0 || c.cfg.capacity <= 0
 }
