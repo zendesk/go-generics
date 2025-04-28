8170ef6365c4d4d75c5740b5be0835afdb1e83b2
Jordan Mance <jordan.mance@zendesk.com>
2025-04-28 12:25:52 -0400

    Wip

--- encryption/go.mod
+++ encryption/go.mod
@@ -5,4 +5,5 @@ go 1.23.0
 
 require (
 	golang.org/x/crypto v0.35.0
+	github.com/zendesk/go-generics/serialize v1.1.2
 )
--- scripts/artifactory_publish.sh
+++ scripts/artifactory_publish.sh
@@ -27,6 +27,8 @@ publish() {
   export JFROG_CLI_OFFER_CONFIG=false
   ~/jfrog rt go-publish go-pkg "$TAG_NAME" --url=https://zdrepo.jfrog.io/zdrepo --user="$ARTIFACTORY_USERNAME" --password="$ARTIFACTORY_API_KEY"
   echo "Publish succeeded!"
+  # sleep to guarantee that the publish save is complete before it possibly being necessary fro the next build
+  sleep 5
 }
 
 # Must publish packages in order based on dependencies.
